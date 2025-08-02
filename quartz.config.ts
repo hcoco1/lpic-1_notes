e8787600fb9d6e2593d6d3daf655df3a6840aa7c
Ivan <arias.ivan@gmail.com>
2025-08-02 16:57:43 +0200

    vercel json added

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "quartz.hcoco1.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
